e8342a15cc1b81b91cdb2b011a045a1132a3e06e
Greg Manco <gregmanco@gmail.com>
2019-07-24 18:08:21 -0400

    Fixed Bands and Town api call

--- bands.js
+++ bands.js
@@ -1,21 +1,29 @@
  var axios = require("axios")
-var moment = require("moment")
+ var moment = require("moment")
+ var fs = require("fs")
 
- var Bands = function() {
-     this.getBand = function(artist){
+ var Bands = function () {
+     this.getBand = function (artist) {
          var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
-         axios.get(URL).then(function(response) {
-            var jsonData = response.data
-            console.log(jsonData)
+         axios.get(URL).then(function (response) {
+             var jsonData = response.data[0]
+             console.log(jsonData)
 
-            var aritstData = [
-                "Name of Venue  " + jsonData.venue.name,
-                "Venue Location " + jsonData.venue.city,
-                "Date of the Event " + moment(jsonData.datetime),
-            ]
-                
-            })
-         }
+             var aritstData = [
+                 "Name of Venue  " + jsonData.venue.name,
+                 "Venue Location " + jsonData.venue.city,
+                 "Date of the Event " + moment(jsonData.datetime).format("MM/DD/YYYY")
+             ].join("\n\n")
+
+             fs.appendFile("log.txt", aritstData, function (err) {
+                 if (err) throw err;
+                 console.log(aritstData);
+
+
+             })
+
+         })
      }
- 
+ }
+ module.exports = Bands;
--- liri.js
+++ liri.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
-require(bands.js)
-
+var key = require("dotenv").config();
+var Bands = require("./bands.js")
 var keys = require("./keys.js");
 
+var band = new Bands();
 var userInput = process.argv.slice(3).join(" ");
 
 //var spotify = new Spotify(keys.spotify);
@@ -12,6 +12,7 @@ var command = process.argv[2]
 
 if( command === "concert-this" ){
     console.log( "Bands in Town API")
+    band.getBand(userInput);
 }
 else if ( command === "spotify-this-song"){
     console.log("Spotify API Command")
